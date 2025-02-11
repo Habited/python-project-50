d749a6298f57085381f24f8909c16a2134bcd78f
Habited <pdskate88@gmail.com>
2025-02-11 20:00:23 +0300

    updated file: __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from difference_calculator.gendiff import generate_diff
 from difference_calculator.json_util import read_json_files
+from tests.test_gendiff import test_generate_diff
 
 
-__all__ = (generate_diff, read_json_files)
+__all__ = (generate_diff, read_json_files, test_generate_diff)
