bee83bfd8ede5044e26565904fe8a3e7a0877521
Habited <pdskate88@gmail.com>
2025-02-12 22:33:17 +0300

    updated files: Makefile, pyci.yml, added file tests/__init__.py

---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+import difference_calculator.gendiff
+from difference_calculator import json_util
+import tests.test_gendiff
+
+
+__all__ = (difference_calculator.gendiff, json_util, tests.test_gendiff)
