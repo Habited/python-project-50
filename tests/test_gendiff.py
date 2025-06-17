cd3b7fcc22893f1de67135b159ae3b4c09e92502
Habited <pdskate88@gmail.com>
2025-06-17 20:37:58 +0300

    removed files: tests/test_data/file3.json, tests/test_data/file3.yaml, tests/test_data/file4.json, tests/test_data/file4.yaml; fixed files: tests/test_data/file1.json, tests/test_data/file2.json, tests/test_data/file1.yaml, tests/test_data/file2.yaml, tests/test_data/test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,61 +1,47 @@
 from gendiff import gendiff
 
 
-def test_generate_diff_json():
-    file = open('tests/test_data/format_flat.txt', 'r')
-    assert gendiff.generate_diff(
-        'tests/test_data/file1.json', 
-        'tests/test_data/file2.json') == file.read()
-
-
-def test_generate_diff_yaml():
-    file = open('tests/test_data/format_flat.txt', 'r')
-    assert gendiff.generate_diff(
-        'tests/test_data/file1.yaml', 
-        'tests/test_data/file2.yaml') == file.read()
-
-
 def test_generate_diff_nested_yaml():
     file = open('tests/test_data/format_stalish.txt', 'r')
     assert gendiff.generate_diff(
-        'tests/test_data/file3.yaml',
-        'tests/test_data/file4.yaml') == file.read()
+        'tests/test_data/file1.yaml',
+        'tests/test_data/file2.yaml', ) == file.read()
 
 
 def test_generate_diff_nested_json():
     file = open('tests/test_data/format_stalish.txt', 'r')
     assert gendiff.generate_diff(
-        'tests/test_data/file3.json',
-        'tests/test_data/file4.json') == file.read()
+        'tests/test_data/file1.json',
+        'tests/test_data/file2.json') == file.read()
 
 
 def test_generate_diff_plain_json():
     file = open('tests/test_data/format_plain.txt', 'r')
     assert gendiff.generate_diff(
-        'tests/test_data/file3.json',
-        'tests/test_data/file4.json', 
+        'tests/test_data/file1.json',
+        'tests/test_data/file2.json', 
         format_name='plain') == file.read()
 
 
 def test_generate_diff_plain_yaml():
     file = open('tests/test_data/format_plain.txt', 'r')
     assert gendiff.generate_diff(
-        'tests/test_data/file3.yaml',
-        'tests/test_data/file4.yaml', 
+        'tests/test_data/file1.yaml',
+        'tests/test_data/file2.yaml', 
         format_name='plain') == file.read()
 
 
 def test_generate_diff_json_yaml():
     file = open('tests/test_data/format_json.txt', 'r')
     assert gendiff.generate_diff(
-        'tests/test_data/file3.yaml',
-        'tests/test_data/file4.yaml', 
+        'tests/test_data/file1.yaml',
+        'tests/test_data/file2.yaml', 
         format_name='json') == file.read()
 
 
 def test_generate_diff_format_json():
     file = open('tests/test_data/format_json.txt', 'r')
     assert gendiff.generate_diff(
-        'tests/test_data/file3.json',
-        'tests/test_data/file4.json',
+        'tests/test_data/file1.json',
+        'tests/test_data/file2.json',
         format_name='json') == file.read()
